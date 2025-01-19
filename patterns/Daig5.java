d1f8cf137fb5fa904257c8b1f7d0a8d5aa36a831
shruti-s0405 <shruti.sundar0405@gmail.com>
2025-01-19 08:18:45 +0530

    Pattern 5 completed

--- patterns/Daig5.java
+++ patterns/Daig5.java
@@ -1,6 +1,6 @@
 public class Daig5 {
     public static void main(String[] args){
-        for(int i=0; i<3; i++){
+        for(int i=0; i<5; i++){
             int n = 5;
             for(int j=0; j<5; j++){
                 System.out.print(n--);
